78b59a035432fa7dd67508787faf7974051b5a97
Sudipta Banik Trisha <43735253+Sudipta-Trisha@users.noreply.github.com>
2019-09-17 20:01:52 +0600

    Add files via upload

---
+++ 1043 -  Triangle Partitioning.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int cnt=0, test;
+    cin>>test;
+    while(test--){
+        double ab,bc,ca,r;
+        cin>>ab>>bc>>ca>>r;
+       
+        double area = sqrt(r/(r+1))*ab;
+        cnt++;
+        printf("Case %d: %.6lf\n",cnt,area);
+    }
+    return 0;
+}
