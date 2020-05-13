3dda9115d571782a5b3516f7d0e696127d6445b3
Sudipta Banik Trisha <sudiptatrisha.19@gmail.com>
2020-05-12 21:36:27 -0700

    Basic sum

---
+++ 1000 - Greetings from LightOJ.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int cnt=0;
+
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--){
+        int a,b;
+        cin>>a>>b;
+        cout<<"Case "<<++cnt<<": "<<a+b<<endl;
+    }
+    return 0;
+}
